ad1e6f02b4547ccc66f0c14ad8bb35cf4928a095
Jesse Kinsella <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 22:00:12 +0000

    added code

---
+++ main.py
@@ -0,0 +1,13 @@
+#add comments to help you remeber what you were thinking
+
+wallet = 40 #starting bal
+
+wallet -= 8 #bought some food
+
+wallet += 40 #I got paid
+
+print(wallet)
+
+
+#'nick'.up() I need to figure out how to upper case this
+
